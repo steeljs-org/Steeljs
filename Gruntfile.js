a1caacddbc4316eceaf932b1b6656ce93d3033dd
王政 <wangzheng4@staff.weibo.com>
2015-11-11 13:58:37 +0800

    bugfix

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,5 +46,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['steel-combine', 'uglify', 'copy']);
+	grunt.registerTask('default', ['steel-combine', 'uglify']);
 };
