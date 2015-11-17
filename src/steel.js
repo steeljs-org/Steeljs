31f37e6f42f61d2e5f7df9aa4894e3af369fbcaf
王政 <wangzheng4@staff.weibo.com>
2015-11-17 11:21:17 +0800

    console.log use core_log

--- src/steel.js
+++ src/steel.js
@@ -59,7 +59,7 @@
     window.scrollTo(0, 0);
     render_run(mainBox, controller);
     core_notice_fire('stageChange', mainBox);
-    console.log("routerChange", mainBox, controller, changeType);
+    core_log("routerChange", mainBox, controller, changeType);
   });
 
   window.steel = steel;
