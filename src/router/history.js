06d19f72eb2dfdb1c77db435b0e754fa96e2c4f5
王政 <wangzheng4@staff.weibo.com>
2015-12-10 01:52:24 +0800

    添加地址管理工具（支持对state进行数据管理，state历史中的索引管理）

---
+++ src/router/history.js
@@ -0,0 +1,66 @@
+/**
+ * 地址管理，负责管理state的数据和当面页面在state历史中的索引位置
+ */
+
+// 当前页面在整个单页面跳转中的索引位置
+var router_history_stateIndex_key = 'stateIndex';
+var router_history_state_data;
+var router_history_state_dataForPush;
+
+router_history_state_init();
+
+core_notice_on('routerChange', router_history_state_init);
+
+//history pushState 及一些处理
+function router_history_pushState(url) {
+    router_history_state_setPush(router_history_stateIndex_key, router_history_getStateIndex() + 1);
+    history.pushState(router_history_stateForPush(), undefined, url);
+    router_history_state_init();
+}
+//history repaceState 及一些处理
+function router_history_replaceState(url) {
+    history.replaceState(router_history_state(), undefined, url);
+}
+//获取当前页面在整个单页面跳转中的索引位置
+function router_history_getStateIndex() {
+    return router_history_state_get(router_history_stateIndex_key, 0);
+}
+//初始化state数据
+function router_history_state_init() {
+    router_history_state_dataForPush = {};
+    router_history_state_data = router_history_state();
+}
+//获取当前的state
+function router_history_state() {
+    return history.state || {};
+}
+//获取下一个将要push页面的state数据
+function router_history_stateForPush() {
+    return router_history_state_dataForPush;
+}
+//获取当前state上的某值
+function router_history_state_get(key, defaultValue) {
+    router_history_state_data = router_history_state();
+    if (key in router_history_state_data) {
+        return router_history_state_data[key];
+    } else if (defaultValue !== undefined) {
+        router_history_state_set(key, defaultValue);
+        return defaultValue;
+    }
+}
+//设置值到缓存中，并不能真正更改history.state
+function router_history_state_set(key, value) {
+    router_history_state_data = {};
+    var state = history.state;
+    if (state) {
+        for (var state_key in state) {
+            router_history_state_data[state_key] = state[state_key];
+        }
+    }
+    router_history_state_data[key] = value;
+    router_history_replaceState(location.href);
+}
+//向下一个state的缓存区域添加数据项 并返回新的数据
+function router_history_state_setPush(key, value) {
+    router_history_state_dataForPush[key] = value;
+}
