a84d91991054b36c432d494cdd1468c08a7dec3c
王政 <wangzheng4@staff.weibo.com>
2016-01-06 16:53:18 +0800

    解决参数id不存在的情况下不能成功处理的bug

--- src/core/nameSpaceFix.js
+++ src/core/nameSpaceFix.js
@@ -5,11 +5,13 @@
  */
 function core_nameSpaceFix(id, basePath) {
     basePath = basePath && core_urlFolder(basePath);
-    if (id.indexOf('.') === 0) {
-        id = basePath ? (basePath + id).replace(/\/\.\//, '/') : id.replace(/^\.\//, '');
-    }
-    while (id.indexOf('../') !== -1) {
-        id = id.replace(/\w+\/\.\.\//, '');
+    if (id) {
+        if (id.indexOf('.') === 0) {
+            id = basePath ? (basePath + id).replace(/\/\.\//, '/') : id.replace(/^\.\//, '');
+        }
+        while (id.indexOf('../') !== -1) {
+            id = id.replace(/\w+\/\.\.\//, '');
+        }
     }
     return id;
 }
