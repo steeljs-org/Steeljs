251166c7377fcb3da73faba9c9b9fcbbb633b925
王政 <wangzheng4@staff.weibo.com>
2016-01-25 20:53:21 +0800

    添加样式加载前预判断的支持

--- src/loader/cssloader.js
+++ src/loader/cssloader.js
@@ -6,26 +6,28 @@
 //import resource/queue
 
 function loader_css(url, callback, load_ID) {
-    var link = core_dom_createElement('link');
     var load_div = null;
     var domID = core_uniqueKey();
     var timer = null;
     var _rTime = 500;//5000毫秒
 
+    load_div = core_dom_createElement('div');
+    core_dom_setAttribute(load_div, 'id', load_ID);
+    core_hideDiv_appendChild(load_div);
+    
+    if (check()) {
+        return;
+    }
+    var link = core_dom_createElement('link');
     core_dom_setAttribute(link, 'rel', 'Stylesheet');
     core_dom_setAttribute(link, 'type', 'text/css');
     core_dom_setAttribute(link, 'charset', 'utf-8');
     core_dom_setAttribute(link, 'id', 'link_' + load_ID);
     core_dom_setAttribute(link, 'href', url);
     head.appendChild(link);
-    load_div = core_dom_createElement('div');
-    core_dom_setAttribute(load_div, 'id', load_ID);
-    core_hideDiv_appendChild(load_div);
 
     timer = function() {
-        if (parseInt(window.getComputedStyle ? getComputedStyle(load_div, null)['height'] : load_div.currentStyle && load_div.currentStyle['height']) === 42) {
-            core_hideDiv_removeChild(load_div);
-            callback(true);
+        if (check()) {
             return;
         }
         if (--_rTime > 0) {
@@ -37,6 +39,14 @@ function loader_css(url, callback, load_ID) {
         }
     };
     setTimeout(timer, 50);
+    function check() {
+        var result = parseInt(window.getComputedStyle ? getComputedStyle(load_div, null)['height'] : load_div.currentStyle && load_div.currentStyle['height']) === 42;
+        if (result) {
+            load_div && core_hideDiv_removeChild(load_div);
+            callback(true);
+        }
+        return result;
+    }
 }
 
 function loader_css_remove(load_ID) {
