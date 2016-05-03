6cb4e32462407083ff5355555ccfd6be8feff132
李晗 <lihan2@staff.weibo.com>
2016-05-03 15:53:22 +0800

    IE8 syntax err

--- src/render/parse.js
+++ src/render/parse.js
@@ -9,24 +9,24 @@ function render_parse(jadeFunStr) {
     var result = [];
     var ret = [];
     var reg = /<[a-z]+([^>]*?s-(child)[^>]*?)>/g;//|tpl|data|css|logic
-    
+
     while (g = reg.exec(jadeFunStr)) {
         var ele = g[1].replace(/\\\"/g, '"');
         var oEle = ele.replace(/\"/g, '').replace(/ /g, '&');
         var eleObj = core_queryToJson(oEle);
         var id = render_base_idMaker();
-        
+
         eleObj['s-id'] = id;
         eleObj['s-all'] = ele;
         result.push(eleObj);
     }
-    
+
     reg = RegExp('(class=\"[^\]*?' + render_stage_scroll_class + '[^\]*?\")');
     if (g = reg.exec(jadeFunStr)) {
         result.push({
             's-stage-scroll': true,
             's-all': g[1].replace(/\\\"/g, '"'),
-            's-id': render_base_idMaker(),
+            's-id': render_base_idMaker()
         });
     }
     return result;
